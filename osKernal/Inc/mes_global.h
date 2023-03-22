c53f9377a6340bc27f4163fc76bd27f041b98cc3
ZheWana <1312560006@qq.com>
2023-03-22 15:12:46 +0800

    Upload Files

---
+++ osKernal/Inc/mes_global.h
@@ -0,0 +1,17 @@
+/**
+ * @file mes_global.h
+ * @author ZheWana (zhewana@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __MES_GLOBAL_H
+#define  __MES_GLOBAL_H
+
+
+
+#endif // !__MES_GLOBAL_H
+
